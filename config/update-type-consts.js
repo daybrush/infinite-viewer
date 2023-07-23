403466c99839e20ac17f57eec36a874c41d9ff2b
Daybrush <younkue.choi@navercorp.com>
2023-07-23 16:10:43 +0900

    fix: fix angular consts

--- config/update-type-consts.js
+++ config/update-type-consts.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const { convertProperties } = require("@daybrush/release/angular");
-const { PROPERTIES, EVENTS } = require("../packages/infinite-viewer/dist/infinite-viewer.cjs.js");
+const { EVENTS, OPTIONS } = require("../packages/infinite-viewer/dist/infinite-viewer.cjs.js");
 
 convertProperties(
     {
-        ANGULAR_INFINITE_VIEWER_INPUTS: PROPERTIES,
+        ANGULAR_INFINITE_VIEWER_INPUTS: OPTIONS,
         ANGULAR_INFINITE_VIEWER_OUTPUTS: EVENTS,
     },
     [
