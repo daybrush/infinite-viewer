96425ea584a0df5487922d6496a61efb1890e2c2
Soeren Schoenbrod <soeren.schoenbrod@outlook.de>
2023-10-29 18:29:59 +0100

    chore: upgrade lit (#66)
    Lit v3 has been released. It is vastly compatibly with Lit v2. Some
    small changes were necessary (e.g. decorators should be imported from
    lit/decorators.js instead of lit-element).

--- packages/lit-infinite-viewer/src/LitInfiniteViewer.ts
+++ packages/lit-infinite-viewer/src/LitInfiniteViewer.ts
@@ -1,4 +1,5 @@
-import { LitElement, html, customElement, property } from "lit-element";
+import { LitElement, html } from "lit";
+import {customElement, property} from 'lit/decorators.js';
 import VanillaInfiniteViewer, {
     InfiniteViewerOptions,
     EVENTS, PROPERTIES, InfiniteViewerMethods, METHODS, OPTIONS
